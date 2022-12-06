bc5cef9084c7cc470107af9a551f876408b39a65
DavidC-DFFF <chessa.david@gmail.com>
2022-12-06 21:38:36 +0100

    HDwallet

--- migrations/1667241345_karma.js
+++ migrations/1667241345_karma.js
@@ -3,7 +3,7 @@ module.exports = async function(deployer) {
  
    await deployer.deploy(
     Karma, 
-    "0x71b7baAf02a51EC4eE253c0aF62721A81C17C1b9",
+    "0xaBC98aa3A20039b5540b4cEC913eF8B7D50f8E74",
     "0xCd0E92f6f9Db77B19910a70aaCE7270D8061Ed38"
     );
    const contract = await Karma.deployed();
